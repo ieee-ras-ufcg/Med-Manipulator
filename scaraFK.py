1b19207fb6b06a4038a679c20f265fd6f7e26d08
GabrielSaant <93060466+GabrielSaant@users.noreply.github.com>
2025-06-13 23:49:48 -0300

    Update scaraFK.py
    falta resolver em relação a z

--- scaraFK.py
+++ scaraFK.py
@@ -1,3 +1,4 @@
+
 from coppeliasim_zmqremoteapi_client import *
 import math
 import numpy as np
@@ -7,81 +8,70 @@ client = RemoteAPIClient()
 sim = client.require('sim')
 
 mtb = sim.getObject("/MTB")
-handles = [
-    sim.getObject("/MTB/axis"),
-    sim.getObject("/MTB/link/axis"),
-    sim.getObject("/MTB/link/axis/link/axis"),
-    sim.getObject("/MTB/link/axis/link/axis/axis")
-]
+joint1 = sim.getObject("/MTB/axis")
+joint2 = sim.getObject("/MTB/link/axis")
+joint3 = sim.getObject("/MTB/link/axis/link/axis")
+joint4 = sim.getObject("/MTB/link/axis/link/axis/axis")
 suctionPad = sim.getObject("/MTB/suctionPad")
 
-# DH parameters
-a1 = 0.71663  # primeiro elo
-a2 = 0.6      # segundo elo
-d1 = 0.1      # altura da base
-d4 = 0.05     # efetuador
+#dh
+a1 = 0.467 #primeiro elo
+a2 = 0.4005  #segundo elo
+d1 = 0.152 #altura da base
+d4 = 0.05 #efetuador
 
-default_pose = [-90*math.pi/180,3*math.pi/180, -120*math.pi/180,-35*math.pi/180]
-final_position = [math.pi/180, math.pi/6, math.pi/2, math.pi/4]
 
-vel = 120 * math.pi/180 
-accel = 40 * math.pi/180
-jerk = 80 * math.pi/180
-maxVel = [vel*math.pi/180] * 5
-maxAccel = [accel*math.pi/180] * 5
-maxJerk = [jerk*math.pi/180] * 5
+final_position = [math.pi/2, math.pi/4, -0.20, math.pi/2]
 
-def moveToConfig(handles, default_pose, maxVel, maxAccel, maxJerk):
-    params = {
-        'joints': handles,
-        'targetPos': default_pose,
-        'vel': maxVel,
-        'accel': maxAccel,
-        'jerk': maxJerk,
-    }
-    sim.moveToConfig(params)
+params = {
+    'joints': [joint1, joint2, joint3, joint4],  
+    'targetPos': final_position,               
+    'maxVel': [3, 3, 2, 3],           
+    'maxAccel': [3, 3, 3, 3],        
+    'maxJerk': [2, 2, 1, 2],                      
+}
+movement_time=5
 
 def forwardKinematics(theta1, theta2, d3, theta4):
     A1 = np.array([
         [math.cos(theta1), -math.sin(theta1), 0, a1 * math.cos(theta1)],
-        [math.sin(theta1), math.cos(theta1), 0, a1 * math.sin(theta1)],
-        [0, 0, 1, d1],
-        [0, 0, 0, 1]
+        [math.sin(theta1),  math.cos(theta1), 0, a1 * math.sin(theta1)],
+        [0,                0,                1, d1],
+        [0,                0,                0, 1]
     ])
-    
+
     A2 = np.array([
         [math.cos(theta2), -math.sin(theta2), 0, a2 * math.cos(theta2)],
-        [math.sin(theta2), math.cos(theta2), 0, a2 * math.sin(theta2)],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]
+        [math.sin(theta2),  math.cos(theta2), 0, a2 * math.sin(theta2)],
+        [0,                0,                1, 0],
+        [0,                0,                0, 1]
     ])
-    
+
     A3 = np.array([
         [1, 0, 0, 0],
         [0, 1, 0, 0],
-        [0, 0, 1, d3],
+        [0, 0, 1, d3],  
         [0, 0, 0, 1]
     ])
-    
+
     A4 = np.array([
         [math.cos(theta4), -math.sin(theta4), 0, 0],
-        [math.sin(theta4), math.cos(theta4), 0, 0],
-        [0, 0, 1, d4],
-        [0, 0, 0, 1]
+        [math.sin(theta4),  math.cos(theta4), 0, 0],
+        [0,                0,                1, d4],
+        [0,                0,                0, 1]
     ])
-    
-    return A1 @ A2 @ A3 @ A4
+ 
+    T = (A1 @ A2 @ A3 @ A4)
 
-sim.startSimulation()
+    return T
+
 
 t_final = forwardKinematics(*final_position)
-final_pos = t_final[0:3, 3]
-print(f"Expected end effector position: {final_pos}")
+final_pos = t_final[0:3,3]
 
-moveToConfig(handles, final_position, maxVel, maxAccel, maxJerk)
-
-
-actual_pos = sim.getObjectPosition(suctionPad, -1)
-print(f"Actual position coppelia: {actual_pos}")
-time.sleep(5)  
-sim.stopSimulation()  
+sim.startSimulation()
+sim.moveToConfig(params)
+time.sleep(5)
+print(f"posição final do efetuador pelo codigo: {final_pos}")
+print(f"posição final do efetuador pelo coppelia: {sim.getObjectPosition(suctionPad, mtb)}")
+time.sleep(60)
