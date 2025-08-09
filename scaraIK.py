5a928f57f8a9b3764bce1dc47215cc1219fd120b
GabrielSaant <93060466+GabrielSaant@users.noreply.github.com>
2025-08-09 13:13:45 -0300

    Update scaraIK.py
    ik basico e potencialmente perigoso

--- scaraIK.py
+++ scaraIK.py
@@ -1,59 +1,48 @@
-from coppeliasim_zmqremoteapi_client import *
+# --- Bibliotecas e Chamada da API ---
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 import math
+import numpy as np
 import time
 
+# --- Conexão e Definições Iniciais ---
 client = RemoteAPIClient()
 sim = client.require('sim')
 
+# --- Parâmetros do Braço ---
+L1 = 0.467
+L2 = 0.4005
 
-L1, L2 = 0.71663, 0.60
-
-mtb = sim.getObject("/MTB")
+# --- Handles dos Objetos ---
 joint1 = sim.getObject("/MTB/axis")
 joint2 = sim.getObject("/MTB/link/axis")
-joint3 = sim.getObject("/MTB/link/axis/link/axis")
-finalScara = sim.getObject("/MTB/suctionPad/LoopClosureDummy2")
-rec = sim.getObject("/Rectangle")
+cube = sim.getObject("/MTB/Rectangle")
 
+joint_handles_list = [joint1, joint2]
 
-def inverse_kinematics(x, y, z):
-    dist = math.hypot(x, y)
-    max_reach = L1 + L2
-    min_reach = abs(L1 - L2)
-    if dist > max_reach or dist < min_reach:
-        return None
-    
+# --- Cinemática Inversa (IK) ---
+def inverse_kinematics(x, y):
     cos_theta2 = (x**2 + y**2 - L1**2 - L2**2) / (2 * L1 * L2)
     cos_theta2 = max(min(cos_theta2, 1), -1)
     theta2 = math.acos(cos_theta2)
+    
     den = L1 + L2 * math.cos(theta2)
     theta1 = math.atan2(y, x) - math.atan2(L2 * math.sin(theta2), den)
-    return theta1, theta2, z
+    
+    return theta1, theta2
+   
 
-
-def move_robot(theta1, theta2, theta3):
+# --- Movimento do Braço ---
+def move_to_target(theta1, theta2):
     sim.setJointTargetPosition(joint1, theta1)
-    time.sleep(0.02)
     sim.setJointTargetPosition(joint2, theta2)
-    time.sleep(0.02)
-    sim.setJointTargetPosition(joint3, theta3)
-    time.sleep(0.02)
 
-
-def follow_rectangle():
-    while True:    
-        target_pos = sim.getObjectPosition(rec, mtb) 
-
-        angles = inverse_kinematics(-target_pos[0], -target_pos[1], target_pos[2])
-
-        if angles:
-            theta1, theta2, theta3 = angles
-            move_robot(theta1, theta2, theta3)
-
-
-
+def cube_following():
+    while True:
+        cube_position = sim.getObjectPosition(cube, -1)
+        angles = inverse_kinematics(cube_position[0], cube_position[1])
+        theta1,theta2 = angles
+        move_to_target(theta1,theta2)
 sim.startSimulation()
-follow_rectangle()
-time.sleep(0.02)
-
+cube_following()
+time.sleep(60)
 sim.stopSimulation()
